d36a7d2fefd00708fae8155b22cba0cb8bc50396
Vimal Mistry <vimal@depixed.com>
2020-02-17 18:42:46 +0530

    fixed

--- src/LaravelAdminCommand.php
+++ src/LaravelAdminCommand.php
@@ -50,7 +50,7 @@ class LaravelAdminCommand extends Command
         if (\App::VERSION() >= '5.2') {
             $this->info("Generating the authentication scaffolding");
             // php artisan ui bootstrap
-            $this->call('ui bootstrap --auth');
+            $this->call('php artisan ui bootstrap --auth');
         }
 
         $this->info("Publishing the assets");
